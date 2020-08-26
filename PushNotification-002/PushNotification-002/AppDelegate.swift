5cf48655f5672decc28c92c868ca0c6de67b7e5d
omrobbie <putu.roby@yahoo.com>
2020-08-26 14:41:14 +0800

    setup notification

--- PushNotification-002/PushNotification-002/AppDelegate.swift
+++ PushNotification-002/PushNotification-002/AppDelegate.swift
@@ -14,10 +14,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        setupNotification(application: application)
         return true
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    private func setupNotification(application: UIApplication) {
+        let center = UNUserNotificationCenter.current()
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+
+        center.requestAuthorization(options: options) { (_, _) in }
+        application.registerForRemoteNotifications()
     }
 }
